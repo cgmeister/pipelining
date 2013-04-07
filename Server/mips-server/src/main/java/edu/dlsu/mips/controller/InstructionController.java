6862d27c1f42ec596b25968f606788fa88cee89e
Rick <heinrichchan@yahoo.com>
2013-04-07 19:25:38 +0800

    instruction controller beta
    changed mvc-dispatcher-servlet.xml to inject pipelineProcessor

--- Server/mips-server/src/main/java/edu/dlsu/mips/controller/InstructionController.java
+++ Server/mips-server/src/main/java/edu/dlsu/mips/controller/InstructionController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import edu.dlsu.mips.domain.Instruction;
-import edu.dlsu.mips.domain.PipelineProcess;
 import edu.dlsu.mips.dto.InstructionDTO;
 import edu.dlsu.mips.exception.JumpAddressException;
 import edu.dlsu.mips.exception.MemoryAddressOverFlowException;
@@ -18,37 +17,31 @@ import edu.dlsu.mips.exception.RegisterAddressOverFlowException;
 import edu.dlsu.mips.exception.StorageInitializationException;
 import edu.dlsu.mips.exception.TrapException;
 import edu.dlsu.mips.processor.PipelineProcessor;
-import edu.dlsu.mips.util.SystemUtils;
 
 @Controller
 @RequestMapping("/Instruction")
 public class InstructionController {
 	
 	private PipelineProcessor pipelineProcessor;
-	private Instruction instruction;
+	
 	
 	@Autowired
 	public void setPipelineProcessor(PipelineProcessor pipelineProcessor){
 		this.pipelineProcessor = pipelineProcessor;
 	}
-	@Autowired
-	public void setInstruction(Instruction instruction){
-		this.instruction = instruction;
-	}
+	
 	
 	
 	@RequestMapping(value = "/send", method = RequestMethod.GET)
 	@ResponseBody
-	public InstructionDTO sendInstruction(@RequestParam("instruction") String instruction){
+	public InstructionDTO recieveInstruction(@RequestParam("instruction") String instruction){
 		
 		InstructionDTO instructionDTO = InstructionDTO.getInstance();
 		instructionDTO.setInstruction(instruction);
-		PipelineProcess pipelineProcess = PipelineProcess.newInstance(instructionDTO.getInstructionSet());
 		
-		this.instruction.setInstruction(instruction);
-		//SystemUtils.addActiveProcess(pipelineProcess);
+		Instruction instructionDomain = Instruction.newInstance(instruction);
 		try {
-			pipelineProcessor.processInstruction(this.instruction);
+			pipelineProcessor.processInstruction(instructionDomain);
 		} catch (JumpAddressException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -71,6 +64,8 @@ public class InstructionController {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
+		
 		return instructionDTO;
 		
 	}
