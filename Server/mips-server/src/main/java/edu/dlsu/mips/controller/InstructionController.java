6c1529611b14c4f440b122b60c48e3d354017d4c
Rick <heinrichchan@yahoo.com>
2013-04-07 17:14:07 +0800

    instruction controller

--- Server/mips-server/src/main/java/edu/dlsu/mips/controller/InstructionController.java
+++ Server/mips-server/src/main/java/edu/dlsu/mips/controller/InstructionController.java
@@ -1,5 +1,25 @@
 package edu.dlsu.mips.controller;
 
-public class InstructionController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import edu.dlsu.mips.dto.InstructionDTO;
+
+@Controller
+@RequestMapping("/Instruction")
+public class InstructionController {
+	
+	@RequestMapping(value = "/send", method = RequestMethod.GET)
+	@ResponseBody
+	public String sendInstruction(@RequestParam("instruction") String instruction){
+		
+		InstructionDTO instructionDTO = InstructionDTO.getInstance();
+		instructionDTO.setInstruction(instruction);
+		return "success";
+		
+	}
+	
 }
--- Server/mips-server/src/main/java/edu/dlsu/mips/dto/InstructionDTO.java
+++ Server/mips-server/src/main/java/edu/dlsu/mips/dto/InstructionDTO.java
@@ -1,5 +1,40 @@
 package edu.dlsu.mips.dto;
 
-public class InstructionDTO {
+import edu.dlsu.mips.domain.InstructionSet;
+import edu.dlsu.mips.exception.JumpAddressException;
+import edu.dlsu.mips.exception.OpcodeNotSupportedException;
+import edu.dlsu.mips.exception.OperandException;
+import edu.dlsu.mips.util.InstructionSetBuilder;
 
+public class InstructionDTO {
+	private static InstructionDTO instructionDTO;
+	
+	static{
+		instructionDTO = new InstructionDTO();
+	}
+	
+	public static InstructionDTO getInstance(){
+		return instructionDTO;
+	}
+	
+	private InstructionSet instructionSet;
+	
+	public void setInstruction(String instruction){
+		try {
+			instructionSet = InstructionSetBuilder.buildInstructionSet(instruction);
+		} catch (JumpAddressException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (OperandException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (OpcodeNotSupportedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public InstructionSet getInstructionSet(){
+		return instructionSet;
+	}
 }
